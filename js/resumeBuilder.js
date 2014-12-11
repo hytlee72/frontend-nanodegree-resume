1ca4618d9b4b46320d0a9a5a824365b8c7f7dcc7
Lee Hoyt <leehoyt@bellsouth.net>
2014-12-11 11:44:44 -0600

    Add 4 Objects Only

---
+++ js/resumeBuilder.js
@@ -0,0 +1,77 @@
+var work = {
+	"jobs": [
+		{
+			"employer": "CompUSA",
+			"title": "Account Manager",
+			"location": "Metairie, La.",
+			"dates": "1997-2000",
+			"description": "Business Sales Account Manager"
+		},
+
+		{
+			"employer": "AT&T",
+			"title": "Client Tech Administrator",
+			"location": "Baton Rouge, La.",
+			"dates": "2000-present",
+			"description": "Local IT Admin for Company-Owned Retail Stores"
+		}
+	]
+}
+
+var projects = {
+	"projects": [
+		{
+			"title": "",
+			"dates": "",
+			"description": "",
+			"images": ""
+		},
+
+		{
+			"title": "",
+			"dates": "",
+			"description": "",
+			"images": ""
+		}	
+	]
+}
+
+var bio = {
+	"name": "Lee Hoyt",
+	"role": "Web Designer",
+	"welcomeMessage": "Welcome to my Darkness!",
+	"contacts": {
+		"mobile": "225-235-3702",
+		"email": "leehoyt@bellsouth.net",
+		"github": "hytlee72",
+		"location": "Baton Rouge, La."
+	},
+
+	"skills": [
+		"awesomeness",
+		"ninjakata master",
+		"pinball wizard",
+		"all-around badass"
+	]
+}
+
+var education = {
+	"schools": [
+	  {
+			"name": "Hammond High School",
+			"location": "Hammond, La.",
+			"degree": "Diploma",
+			"dates": "1987-1990",
+			"url": "http://www.tangischools.org/hhms"
+		}
+	],
+
+	"onlineCourses": [
+		{
+			"title": "Front-End Web Design Nanodegree",
+			"school": "Udacity",
+			"dates": "11/2014-present",
+			"url": "http://www.udacity.com"
+		}
+	]
+}
